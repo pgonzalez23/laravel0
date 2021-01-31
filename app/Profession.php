7d816987a24703c35ac53edafe25d5aa0c5f5882
root <pgonzalez23@alu.ucam.edu>
2021-01-31 11:13:30 +0000

    Fin clase 14

--- app/Profession.php
+++ app/Profession.php
@@ -9,4 +9,6 @@ class Profession extends Model
     //
     //public $timestamps = false; PARA QUE NO SALGA FECHA DE CREATE Y UPDATE EN BD.
 
+    protected $fillable = ['title']; //permite la asignacion masiva (elemento o elementos que se añaden dentro de un create)
+
 }
